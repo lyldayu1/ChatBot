d10ee0cdfb2f4ad777c2dc6e89f34013081b5c6b
Zhenghao Li <zhenghaoli@dhcp-v041-167.mobile.uci.edu>
2018-10-17 14:45:21 -0700

    updatezzz

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
-
-app.set('port', (process.env.PORT || 5000))
+const token = "EAAFV4wUyJZAwBAC9ZANzbez1SqUeQ3a3K9gDyI6SSkxrlhyct42oNCcDE3W4yUPDTQYjqV5AXDfe9IHePmPzZBwC4LRHHQrtbzH1wvCzf1ahXNT5cbxJRqZBeobuOTXcFLPiPtqNvR14IQt8KKG6Xtg3ivBMaZAkhnhT4XFUGhgZDZD"
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
@@ -13,68 +12,92 @@ app.use(bodyParser.urlencoded({extended: false}))
 // Process application/json
 app.use(bodyParser.json())
 
-
-
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send('Error, wrong token')
-})
-
 // Spin up the server
 app.listen(app.get('port'), function() {
 	console.log('Express server is running at port', app.get('port'))
 })
 
-app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-	    let event = req.body.entry[0].messaging[i]
-	    let sender = event.sender.id
-	    if (event.message && event.message.text) {
-		    let text = event.message.text
-		    //sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-	    }
-		// con.connect(function(err) {
-		//   if (err) throw err;
-		//   console.log("Connected!");
-		// });
-		// var sql = "INSERT INTO Orders(UID, Time, Content) VALUES(?, ?, ?)";
-		// var uid = sender
-		// var time = Date.Now()
-		// var txt = text
-		// con.query(sql, (uid, time, txt), function(err, result){
-		// 	if(err) throw err;
-		// 	console.log("added");
-		// });
+
+app.set('port', (process.env.PORT || 5000))
 
 
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "yuliangzz"
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
     }
-    res.sendStatus(200).send("hello");
-})
+  }
+});
 
-const token = "EAAFV4wUyJZAwBAC9ZANzbez1SqUeQ3a3K9gDyI6SSkxrlhyct42oNCcDE3W4yUPDTQYjqV5AXDfe9IHePmPzZBwC4LRHHQrtbzH1wvCzf1ahXNT5cbxJRqZBeobuOTXcFLPiPtqNvR14IQt8KKG6Xtg3ivBMaZAkhnhT4XFUGhgZDZD"
 
-function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-	    url: 'https://graph.facebook.com/v2.6/me/messages',
-	    qs: {access_token:token},
-	    method: 'POST',
-		json: {
-		    recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-		    console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-		    console.log('Error: ', response.body.error)
-	    }
-    })
-}
+
+
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+ 
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
+
+// function sendTextMessage(sender, text) {
+//     let messageData = { text:text }
+//     request({
+// 	    url: 'https://graph.facebook.com/v2.6/me/messages',
+// 	    qs: {access_token:token},
+// 	    method: 'POST',
+// 		json: {
+// 		    recipient: {id:sender},
+// 			message: messageData,
+// 		}
+// 	}, function(error, response, body) {
+// 		if (error) {
+// 		    console.log('Error sending messages: ', error)
+// 		} else if (response.body.error) {
+// 		    console.log('Error: ', response.body.error)
+// 	    }
+//     })
+// }
 
 var mysql = require('mysql');
 
