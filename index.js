7661aa788a4c77ee8665b3501716e5c969828ae2
lyldayu <lyldayu@gmail.com>
2018-10-17 15:35:52 -0700

    judge success

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/webhook', (req, res) => {
       
       // Responds with the challenge token from the request
       console.log('WEBHOOK_VERIFIED');
-      res.status(200).send("success");
+      res.status(200).send(challenge);
     
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
