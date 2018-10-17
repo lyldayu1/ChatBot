012e4c91ee2d0e5cbd4fd937199ca0d3dc74852b
Zhenghao Li <zhenghaoli@dhcp-v041-167.mobile.uci.edu>
2018-10-17 14:35:49 -0700

    updatezzz

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/webhook/', function (req, res) {
 	    let sender = event.sender.id
 	    if (event.message && event.message.text) {
 		    let text = event.message.text
-		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+		    //sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
 	    }
 		// con.connect(function(err) {
 		//   if (err) throw err;
