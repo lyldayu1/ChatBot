ac605ad32aa8d8f6a5e8fc8087a2b07c13c3c4a3
lyldayu <lyldayu@gmail.com>
2018-10-20 21:07:54 -0700

    connect sql

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 5000, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 3000, () => console.log('webhook is listening'));
 
 function connectSql(){
   con.connect(function(err) {
