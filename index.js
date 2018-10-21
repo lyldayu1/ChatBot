38100a0f056ac06007b750029282153756a44465
lyldayu <lyldayu@gmail.com>
2018-10-20 21:17:10 -0700

    connect sql

--- index.js
+++ index.js
@@ -17,15 +17,17 @@ app.listen(process.env.PORT || 3000, () => console.log('webhook is listening'));
 
 function connectSql(){
   con.connect(function(err) {
-  if (err)throw err;
-  console.log("Connected!")
-  var sql="CREATE TABLE customers"+count+" (name VARCHAR(255), address VARCHAR(255))";
-  count++;
-  con.query(sql,function(err,result){
-  if(err)throw err;
-    console.log("Table created");
-  });
-
+  if (err){
+    console.log(err);
+  }else{
+    console.log("Connected!")
+    var sql="CREATE TABLE customers"+count+" (name VARCHAR(255), address VARCHAR(255))";
+    count++;
+    con.query(sql,function(err,result){
+    if(err)throw err;
+      console.log("Table created");
+    });
+  }
 });
 }
 
