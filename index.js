f88490aa5628400be22528f5049d0cc020c99444
lyldayu <lyldayu@gmail.com>
2018-10-20 20:38:09 -0700

    connect sql

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.post('/webhook', (req, res) => {
     //   let webhook_event = entry.messaging[0];
     //   console.log(webhook_event);
     // });
-    connectSql();
+    //connectSql();
      res.sendStatus(200);
   // } else {
   //   // Returns a '404 Not Found' if event is not from a page subscription
@@ -110,6 +110,23 @@ var con = mysql.createConnection({
 	database: "ChatBot"
 });
 var count=0;
+con.connect(function(err) {
+  if (err)
+  {
+    console.log("ERROR in connection to DB")
+  }
+  else
+  {
+    console.log("Connected!")
+    var sql="CREATE TABLE customers"+count+" (name VARCHAR(255), address VARCHAR(255))";
+    count++;
+    con.query(sql,function(err,result){
+      if(err)throw err;
+      console.log("Table created");
+    })
+  }
+
+});
 function connectSql(){
   con.connect(function(err) {
   if (err)
